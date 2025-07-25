01c471fceab1c1f08df139fc613b056c089e41fb
TutlaMC <ayaanibrahim1110@gmail.com>
2025-07-25 22:34:39 +0530

    modified:   src/tusk/node.py

--- src/tusk/node.py
+++ src/tusk/node.py
@@ -1,25 +1,6 @@
 class Node:
     def __init__(self, type_, name, values, auto_eval=False):
+        self.name = name
         self.type = type_
-        self.auto_eval = False
-        
-        if self.type == "3n":
-            self.value1 = values[0]
-            self.operator = values[1]
-            self.value2 = values[2]
 
-            self.left_node = self.value
-            self.operator_node = self.operator
-            self.right_node = self.value2
-        elif self.type == "2n":
-            self.value1 = values[0]
-            self.value2 = values[1]
-
-            self.left_node = self.value
-            self.right_node = self.operator
-        elif self.type == "1n":
-            self.value = values[0]
-            self.left_node = self.value
-        else: raise Exception("Node type not found")
-
-# i have no clue what this is so js ignore this class, everything would still work if u didnt use this Node as the base NODe
+# okay now i realise this shit was from the prototype 3 months back
