d513113bd80d180e30bc693fcb8994e5b1fa4ecf
TutlaMC <ayaanibrahim1110@gmail.com>
2025-07-31 22:43:36 +0530

    modified:   src/tusk/nodes/base/return_node.py

--- src/tusk/nodes/base/return_node.py
+++ src/tusk/nodes/base/return_node.py
@@ -6,8 +6,6 @@ from tusk.nodes.expressions import ExpressionNode
 class ReturnNode(Node):
     def __init__(self, token:Token):
         self.interpreter = token.interpreter
-        self.auto_eval = True
-        self.type="1en"
         self.token = token
 
     async def create(self):
