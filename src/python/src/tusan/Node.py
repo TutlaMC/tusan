6d96dedf667fcb7e6a406d402bd5aa06aee065fd
TutlaMC <ayaanibrahim1110@gmail.com>
2026-04-26 02:41:05 +0530

    more clean

--- src/python/src/tusan/Node.py
+++
@@ -1,9 +0,0 @@
-from tusan.lexer.Token import *
-
-class Node:
-    def __init__(self, type_, name, translation=""):
-        self.name = name
-        self.type = type_
-        self.translation = ""
-
-# okay now i realise this shit was from the prototype 3 months back
--- src/python/src/tusan/interpreter/InterpreterData.py
+++
@@ -1,5 +0,0 @@
-class InterpreterData:
-    def __init__(self):
-        pass
-    def setup():
-        pass
